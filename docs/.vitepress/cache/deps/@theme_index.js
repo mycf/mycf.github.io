f8b2033a701f805e881bf5f0348dfa196a37cac6
喻超凡 <36999825+mycf@users.noreply.github.com>
2025-07-09 05:35:32 +0000

    ok

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,38 +9,38 @@ import {
 } from "./chunk-DDXJJ377.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default5 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
-import { default as default6 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default7 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default9 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default10 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default11 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default12 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
-import { default as default13 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default14 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default15 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default16 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
-import { default as default17 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default18 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default19 } from "/Users/yuchaofan/Documents/code/my-website/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default5 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
+import { default as default6 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default7 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default9 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default10 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default11 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default12 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
+import { default as default13 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default14 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default15 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default16 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import { default as default17 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default18 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default19 } from "/workspaces/mycf.github.io/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
 
 // node_modules/vitepress/dist/client/theme-default/composables/local-nav.js
 import { onContentUpdated } from "vitepress";

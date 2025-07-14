b6f27b4f81be8ec108c240248652b4200c731442
ycfan <y_cfan@163.com>
2025-07-14 21:39:48 +0800

    ok

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import Excalidraw from './Excalidraw.vue'
+import Excalidraw from './excalidraw.vue'
 // import { Excalidraw } from "@excalidraw/excalidraw";
 export default {
   extends: DefaultTheme,
